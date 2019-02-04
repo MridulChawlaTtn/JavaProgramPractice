8bdcc5126544ffd27e4065c17ccf156c93d7db9a
Mridul Chawla <mridul.chawla@tothenew.com>
2019-02-04 17:14:23 +0530

    Project started

---
+++ src/main/java/StringsProgram/FirstNonRepeatingChar.java
@@ -0,0 +1,38 @@
+package StringsProgram;
+
+public class FirstNonRepeatingChar {
+
+    public static void main(String[] args) {
+        System.out.printf("First Non Repeating character in : %s  is : %s \n" , null, getFirstNonRepeatingCharacter(null));
+        System.out.printf("First Non Repeating character in : %s  is : %s \n" , "", getFirstNonRepeatingCharacter(""));
+        System.out.printf("First Non Repeating character in : %s  is : %s \n" , "fsfdsfdsf", getFirstNonRepeatingCharacter("fsfdsfdsf"));
+        System.out.printf("First Non Repeating character in : %s  is : %s \n" , "I am disco dancer", getFirstNonRepeatingCharacter("I am disco dancer"));
+    }
+
+
+    private static Character getFirstNonRepeatingCharacter(String string) {
+        if (string == null || string.isEmpty()) {
+            return null;
+        }
+        char[] chars = string.toCharArray();
+        boolean flag;
+        Character result = null;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != 0) {
+                flag = true;
+                char matcher = chars[i];
+                for (int j = i + 1; j < chars.length; j++) {
+                    if (matcher == chars[j]) {
+                        flag = false;
+                        chars[j] = 0;
+                    }
+                }
+                if (flag) {
+                    result = chars[i];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
