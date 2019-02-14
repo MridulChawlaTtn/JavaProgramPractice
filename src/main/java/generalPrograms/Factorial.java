039300384dd1f30d3c3cd87aa537b1ddb8ef0572
Mridul Chawla <mridul.chawla@tothenew.com>
2019-02-14 13:12:47 +0530

    Factorial program improvement

--- src/main/java/generalPrograms/Factorial.java
+++ src/main/java/generalPrograms/Factorial.java
@@ -1,24 +1,37 @@
 package generalPrograms;
 
 public class Factorial {
-    public static void factorialByLoop(int n) {
-        int i, fact = 1;
-        for (i = 1; i <= n; i++) {
-            fact = fact * i;
+    public static int factorialByLoop(int n) {
+        if (n == 0) {
+            return 1;
         }
-        System.out.println("Factorial of " + n + " is: " + fact);
+        else if(n<0){
+            throw new ArithmeticException("The factorial of negative number is not possible.");
+        }
+        else {
+            int fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact = fact * i;
+            }
+            return fact;
+        }
     }
 
     public static int factorialByRecursion(int n) {
-        if (n == 0)
+        if (n == 0) {
             return 1;
-        else return (n * factorialByRecursion(n - 1));
+        }
+        else {
+            return (n * factorialByRecursion(n - 1));
+        }
     }
 
     public static void main(String args[]) {
         int fact;
         int number = 4; //It is the number to calculate factorial
+        fact = factorialByLoop(number);
+        System.out.println("Factorial of " + number + " by Loop method is: " + fact);
         fact = factorialByRecursion(number);
-        System.out.println("Factorial of " + number + " is: " + fact);
+        System.out.println("Factorial of " + number + " by Recursion method is: " + fact);
     }
 }
