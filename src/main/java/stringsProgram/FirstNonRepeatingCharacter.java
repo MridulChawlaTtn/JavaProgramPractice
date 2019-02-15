ce1a5e0aca0bb1a572572f34d599f602ed3a8ce8
mridul <mridul.chawla@tothenew.com>
2019-02-15 23:13:54 +0530

    Reformatting of the file FirstNonRepeatingCharacter.

--- src/main/java/stringsProgram/FirstNonRepeatingCharacter.java
+++ src/main/java/stringsProgram/FirstNonRepeatingCharacter.java
@@ -36,11 +36,25 @@ public class FirstNonRepeatingCharacter {
     } /* * Using HashMap to find first non-repeated character from String in Java. * Algorithm : * Step 1 : Scan String and store count of each character in HashMap * Step 2 : traverse String and get count for each character from Map. * Since we are going through String from first to last character, * when count for any character is 1, we break, it's the first * non repeated character. Here order is achieved by going * through String again. */
 
     public static char firstNonRepeatedCharacter(String word) {
-        HashMap<Character,Integer> scoreboard = new HashMap<Character,Integer>();
-    // build table [char -> count]
-        for (int i = 0; i < word.length(); i++) { char c = word.charAt(i); if (scoreboard.containsKey(c)) { scoreboard.put(c, scoreboard.get(c) + 1); } else { scoreboard.put(c, 1); } }
+        HashMap<Character, Integer> scoreboard = new HashMap<Character, Integer>();
+        // build table [char -> count]
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (scoreboard.containsKey(c)) {
+                scoreboard.put(c, scoreboard.get(c) + 1);
+            } else {
+                scoreboard.put(c, 1);
+            }
+        }
         // since HashMap doesn't maintain order, going through string again
-        for (int i = 0; i < word.length(); i++) { char c = word.charAt(i); if (scoreboard.get(c) == 1) { return c; } } throw new RuntimeException("Undefined behaviour"); }
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (scoreboard.get(c) == 1) {
+                return c;
+            }
+        }
+        throw new RuntimeException("Undefined behaviour");
+    }
 
-        //Read more: https://javarevisited.blogspot.com/2014/03/3-ways-to-find-first-non-repeated-character-String-programming-problem.html#ixzz5esEa6VNC
+    //Read more: https://javarevisited.blogspot.com/2014/03/3-ways-to-find-first-non-repeated-character-String-programming-problem.html#ixzz5esEa6VNC
 }
