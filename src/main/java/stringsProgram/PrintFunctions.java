565d1cea203b5ea4285e41be83a38361e82a8793
mridul <mridul.chawla@tothenew.com>
2019-03-01 17:59:32 +0530

    Added two files: Repeated and missing number in array and String occurence in List.

--- src/main/java/stringsProgram/PrintFunctions.java
+++ src/main/java/stringsProgram/PrintFunctions.java
@@ -4,7 +4,12 @@ import java.util.Calendar;
 
 public class PrintFunctions {
     public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();
-        System.out.format("%tD", c);
+//        Calendar c = Calendar.getInstance();
+//        System.out.format("%tD", c);
+        String s1 = new String("Mridul");
+        String s2 = new String("Mridul");
+        System.out.println(s1 == s2);
+//        System.out.println(s1.hashCode());
+//        System.out.println(s2.hashCode());
     }
 }
